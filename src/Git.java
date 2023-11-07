f64447600c6dcd5489ce6d7cf1990762da0613c7
hassa <hassa@192.168.1.12>
2023-11-07 09:39:32 -0800

    First Commit...

---
+++ src/Git.java
@@ -0,0 +1,12 @@
+
+public class Git {
+	public int add(int a, int b) {
+		return a+b;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Git obj = new Git();
+		System.out.println(obj.add(7, 8));
+	}
+
+}
